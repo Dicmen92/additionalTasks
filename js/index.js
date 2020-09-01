52add75d21db4bebd0daf673360587b42c41fbcb
Dmitriy Babich <babich-d@mail.ru>
2020-09-01 20:52:16 +0600

    Выполнил задание №2 основного урока №15

--- js/index.js
+++ js/index.js
@@ -1,36 +1,17 @@
 'use strict';
 
-class DomElement {
-  constructor(
-    selector = "#div",
-    height = "400px",
-    width = "400px",
-    bg = "green",
-    fontSize = "24px"
-  ) {
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
+class First {
+  hello(){
+    console.log('Привет, я метод консоля!');
   }
 };
 
-DomElement.prototype.createElem = function () {
-  let block;
-    this.selector.trim().slice(0, 1) == "." ? 
-    block = document.createElement("div") : 
-    this.selector.trim().slice(0, 1) == "#" ? 
-    block = document.createElement("p") :
-    alert("Необходимо начинать строку с '.' или '#' ");
-
-  (this.selector.charAt(0) == '.') ? block.setAttribute('class', this.selector.slice(1)) : block.setAttribute('id', this.selector.slice(1));
-
-  block.textContent = "Обязательное задание урока №14 (часть первая)";  
-  block.style.cssText = `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize}; text-align: center;`;
-  document.body.append(block);
+class Second extends First{
+  hello(){
+    super.hello();
+    console.log('А я наследуемый метод!');
+  }  
 };
 
-const domBlock = new DomElement("#best", "200px", "auto", "red", "30px");
-
-domBlock.createElem();
+const check = new Second();
+check.hello();
